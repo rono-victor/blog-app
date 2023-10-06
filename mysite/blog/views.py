8fb92afe2611e03d10fb6d15989a4d8f836fb834
rono-victor <ronovictor40@gmail.com>
2023-10-06 22:43:32 +0300

    blog html

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,8 +8,9 @@ from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request,'blog/post/list.html',{'posts':posts})
+    return render(request,'blog/blog/post/list.html',{'posts':posts})
 
+from django.http import Http404
 
 def post_detail(request, id):
     post= get_object_or_404(Post, id=id,status=Post.Status.PUBLISHED)
@@ -17,5 +18,5 @@ def post_detail(request, id):
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404 ("No post found.")
-    return render(request,'blog/post/detail.html', {'post':post})
+    return render(request,'blog/blog/post/detail.html', {'post':post})
 
